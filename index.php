35b2ca91015103428801a35d951b72229b5fe092
AgostaSalvatore <agosta.boolean@gmail.com>
2025-07-23 15:50:54 +0200

    feat: add Genre class and adult film property to Movie class with protected access

--- index.php
+++ index.php
@@ -2,20 +2,45 @@
 
 class Movie
 {
-    public $title;
-    public $releaseDate;
-    public $genre;
+    protected $title;
+    protected $releaseDate;
+    protected $adultFilm;
+    protected $genre;
 
-    function __construct($_title, $_releaseDate, $_genre)
+    function __construct($_title, $_releaseDate, $_adultFilm, Genre $_genre)
     {
         $this->title       = $_title;
         $this->releaseDate = $_releaseDate;
+        $this->adultFilm   = $_adultFilm;
         $this->genre       = $_genre;
+    }
+
+    public function isForAdult()
+    {
+        return $this->adultFilm == true;
     }
 }
 
-$vForVendetta = new Movie('V For Vendetta', 2005, 'Action');
-$titanic      = new Movie('Titanic', 1997, 'Romantic');
+class Genre
+{
+    protected $genre;
+
+    function __construct($_genre)
+    {
+        $this->genre = $_genre;
+    }
+
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+}
+
+$action   = new Genre('Action');
+$romantic = new Genre('Romantic');
+
+$vForVendetta = new Movie('V For Vendetta', 2005, true, $action);
+$titanic      = new Movie('Titanic', 1997, false, $romantic);
 
 var_dump($vForVendetta);
 
